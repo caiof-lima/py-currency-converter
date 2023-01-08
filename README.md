cdf5bc517a213823f207ca87f6fbcc22350f31ea
Caio Lima <dev.caiolima@gmail.com>
2023-01-07 21:39:34 -0300

    base project

---
+++ README.md
@@ -0,0 +1,9 @@
+# A simple Python currency converter
+
+This script get the current Google Finance quote from USD to BRL using **urllib** native module, without any external packages or libs.
+
+At this moment, only USD to BRL conversion is created, but is in my plans to add some features like other currencies, decimal and thousand dynamic separator, etc.
+
+## How to use
+1. Run `python usd_brl.py ` and put the value that you want to convert. Example: `python3 usd_brl.py 1000,00`
+2. Enjoy!
---
+++ usd_brl.py
@@ -0,0 +1,25 @@
+import re
+import sys
+from urllib.request import urlopen
+
+
+def fetch_USD_quote(value: float):
+    url = "https://www.google.com/finance/quote/USD-BRL"
+    content = urlopen(url).read()
+    search = re.search('class="YMlKec fxKbKc">(.*?)<', content.decode('utf-8'))
+    if not search or not search.group(1):
+        raise Exception('Not possible to get USD Quote, try again later')
+
+    usd_value = float(search.group(1))
+    brl_currency = value * usd_value
+    return brl_currency 
+
+if __name__ == '__main__':
+    try:
+        input_val = sys.argv[1]
+    except:
+        raise Exception('A value is required to convert the currency')
+    
+    value_number = float(re.sub("[^0-9.]", "", re.sub( ",", ".", input_val)))
+    result = fetch_USD_quote(value_number)
+    print('\nOutput: {:.2f}'.format(result))
